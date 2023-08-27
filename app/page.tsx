4eac8d6b292cde0a17937c7d5648e5d5855f1055
Ayanoooo <marthawij23@gmail.com>
2023-08-28 00:01:26 +0700

    Import Home To MainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,9 @@
-
+import Home from './Home/page'
 
 export default function page() {
   return (
     <>
-      <header className='bg-[#010a5e] fixed top-0 left-0 w-full z-50'>
-        <nav>
-          <div>
-            <a href="#">
-              Bur <span>Ger</span>
-            </a>
-          </div>
-
-          <div>
-            <ul>
-              <li>
-                <a href="">Home</a>
-              </li>
-              <li>
-                <a href="">About</a>
-              </li>
-              <li>
-                <a href="">Menu</a>
-              </li>
-              <li>
-                <a href="">Review</a>
-              </li>
-              <li>
-                <a href="">Contact</a>
-              </li>
-            </ul>
-          </div>
-
-        </nav>
-      </header>
-      {/* End Header */}
-
-      {/* Start Section */}
-      <section>
-        
-      </section>
-
-
+      <Home />
     </>
   )
 }
